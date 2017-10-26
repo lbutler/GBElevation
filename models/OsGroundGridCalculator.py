5d9b6383f56b70d784749cdf01c1d0043be2ba83
lbutler <lukepbutler@gmail.com>
2017-10-26 16:16:41 -0400

    OSGroundGridCalculator added
    Simple calculations for elevations at intersection of grid

---
+++ models/OsGroundGridCalculator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from qgis.core import QgsVectorLayer, QgsSpatialIndex, QgsPoint, QgsFeatureRequest
+
+class OsGroundGridCalculator:
+
+    def __init__(self, gridLayer, gridInterval):
+
+        self.gridLayer = gridLayer
+        self.gridInterval = gridInterval
+        self._gridIndex = QgsSpatialIndex( self.gridLayer.getFeatures() )
+
+
+    def calculateElevation(self, x, y):
+
+        if self._isAtIntersection(x, y):
+            return self._getIntersectionElevation(x,y)
+        else:
+            return 9999
+
+    def _getIntersectionElevation(self, x, y):
+
+        nearest = self._gridIndex.nearestNeighbor(QgsPoint(x, y), 1)
+        f = self.gridLayer.getFeatures(request=QgsFeatureRequest(nearest[0]))
+        for g in f:
+            intersectElevation = g.attribute("HEIGHT")
+
+        return intersectElevation
+
+    def _isAtIntersection(self, x, y):
+
+        if x % self.gridInterval == 0 and y % self.gridInterval == 0:
+            return True
+        else:
+            return False
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from OsTileLocator import OsTileLocator
+from OsGroundGridCalculator import OsGroundGridCalculator
